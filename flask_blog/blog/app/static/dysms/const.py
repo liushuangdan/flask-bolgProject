44ac8f3a67ad7e7ad9459bfea0638b418ad9713b
liushuangdan <43132653+liushuangdan@users.noreply.github.com>
2020-07-09 14:12:00 +0800

    Update const.py

--- flask_blog/blog/app/static/dysms/const.py
+++ flask_blog/blog/app/static/dysms/const.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 # ACCESS_KEY_ID/ACCESS_KEY_SECRET 根据实际申请的账号信息进行替换
-ACCESS_KEY_ID = "xxxxxxxxxxxxxxxxxxxxx"
-ACCESS_KEY_SECRET = "ZMq6887gWQfOg3ebgVHCFd0Mb3U5ch"
+ACCESS_KEY_ID = ""
+ACCESS_KEY_SECRET = ""
